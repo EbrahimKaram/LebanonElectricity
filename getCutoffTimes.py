5f2a095825c6deb82dc77c20807bf7f8bb85a9f5
EbrahimKaram <ebrahim_karam@hotmail.com>
2022-01-04 08:47:19 +0200

    Cleaning up the scipt Starting to make this ready to do the 900 so requests

--- getCutoffTimes.py
+++ getCutoffTimes.py
@@ -4,30 +4,35 @@ from datetime import date, timedelta
 import json
 
 
-def getDataForExit(station_ID, exit_ID):
+def getDataForExit(station_ID, exit_ID, days_back=15):
     today = pd.to_datetime("today")
     print(today.strftime('%Y-%m-%d'))
-    two_weeks_ago = today - timedelta(days=15)
+    two_weeks_ago = today - timedelta(days=days_back)
     print(two_weeks_ago.strftime('%Y-%m-%d'))
     url = 'https://www.edl.gov.lb/feedingdata.php'
 
     myobj = {'a_substations': station_ID, 'a_feeders': exit_ID,
              'd1': two_weeks_ago.strftime('%Y-%m-%d'), 'd2': today.strftime('%Y-%m-%d'), 'mode': 'load', 'actpre': 'act'}
     x = requests.post(url, data=myobj, verify=False)
-    print(x.text)
-    with open('Cuttoff Times/Baskinta.json', 'w', encoding="utf-8") as f:
-        f.write(x.text)
+
+    # print(x.text)
+    return x.text
+    # with open('Cuttoff Times/Baskinta.json', 'w', encoding="utf-8") as f:
+    #     f.write(x.text)
 
 
 if __name__ == '__main__':
     # 162,1D,10,البوشرية
-    # the one above didn't have any data published. I prsume that the exit is just dead
+    # the one above didn't have any data published. I presume that the exit is just dead or didn't have data for the period
     # 305	بسكنتا	18	بكفيا
 
     station_ID = 18
     exit_ID = 305
     reader = open("Cuttoff Times/Baskinta.json", encoding='utf-8')
     data = json.load(reader)
+    if(data["list_feeders"]["dateoffeeding"] == null):
+        print("We need to skip this one")
+        continue
     df = pd.DataFrame(data["list_feeders"])
 # Reference
 # https://pandas.pydata.org/docs/reference/api/pandas.wide_to_long.html
